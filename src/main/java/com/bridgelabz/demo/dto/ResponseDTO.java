9655d2490803df96f74236a4016192b6a253a277
AtifNusrath <mohammedatif90@gmail.com>
2021-11-22 23:10:27 +0530

    Added: Lombok library to Auto generate getter setter

--- src/main/java/com/bridgelabz/demo/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/demo/dto/ResponseDTO.java
@@ -1,27 +1,23 @@
 package com.bridgelabz.demo.dto;
 
+import java.util.List;
+
+import com.bridgelabz.demo.model.EmployeePayrollData;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDTO {
+	
 	private String message;
 	private Object data;
-	public ResponseDTO(String message, Object data) {
-		super();
-		this.message = message;
-		this.data = data;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
-		this.data = data;
-	}
-	@Override
-	public String toString() {
-		return "ResponseDTO [message=" + message + ", data=" + data + "]";
-	}
+	
+	public ResponseDTO(String message,Object data) {
+        this.message = message;
+        this.data = data;
+    }
 }
--- src/main/java/com/bridgelabz/demo/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/demo/model/EmployeePayrollData.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.demo.model;
 import com.bridgelabz.demo.dto.EmployeePayrollDTO;
+import lombok.Data;
 
+@Data
 public class EmployeePayrollData {
 	private int employeeId;
 	private String name;
 	private long salary;
 	
 	public EmployeePayrollData() {
-		// TODO Auto-generated constructor stub
+	
 	}
 	public EmployeePayrollData(int employeeId, String name, long salary) {
 		super();
@@ -21,22 +23,5 @@ public class EmployeePayrollData {
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
 	}
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
+	
 }

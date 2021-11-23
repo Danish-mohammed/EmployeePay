3232f2459f6884eae843e7812f27c85ef543d5e2
AtifNusrath <mohammedatif90@gmail.com>
2021-11-23 10:54:52 +0530

    Added: Creating application properties for dev and production

--- src/main/java/com/bridgelabz/demo/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/demo/EmployeePayrollApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootApplication
@@ -10,8 +11,10 @@ public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Payroll App");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-		log.info("Employee Payroll App started");
-	}
-
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+	       log.info("Employee Payroll App Started in {} Environment",
+	               context.getEnvironment().getProperty("environment"));
+	       log.info("Employee Payroll DB User is {}",
+	               context.getEnvironment().getProperty("spring.datasource.username"));
+	    }
 }

dd226fe8437b5d228f7c20db313913c24564f047
AtifNusrath <mohammedatif90@gmail.com>
2021-11-23 17:05:38 +0530

    Refactore the code for custom exception

--- src/main/java/com/bridgelabz/demo/Service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/demo/Service/EmployeePayrollService.java
@@ -5,42 +5,44 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import com.bridgelabz.demo.dto.EmployeePayrollDTO;
 import com.bridgelabz.demo.model.EmployeePayrollData;
+import com.bridgelabz.demo.Exceptions.EmployeePayrollException;
 
 @Service
-public class EmployeePayrollService implements IEmployeePayrollService{
+public class EmployeePayrollService implements IEmployeePayrollService {
 
-    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
-	 @Override
-	    public List<EmployeePayrollData> getEmployeePayrollData() {
-		 return employeePayrollList;
-	    }
+	@Override
+	public List<EmployeePayrollData> getEmployeePayrollData() {
+		return employeePayrollList;
+	}
 
-	    @Override
-	    public EmployeePayrollData getEmployeePayrollDataById(int employeeId) {
-	    	return employeePayrollList.get(employeeId-1);
-	    }
+	@Override
+	public EmployeePayrollData getEmployeePayrollDataById(int employeeId) {
+		return employeePayrollList.stream().filter(empData -> empData.getEmployeeId() == employeeId).findFirst()
+				.orElseThrow(() -> new EmployeePayrollException("Employee Not found"));
+	}
 
-	    @Override
-	    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
-	    	EmployeePayrollData empData = null;
-	        empData = new EmployeePayrollData(employeePayrollList.size()+1,empPayrollDTO);
-	        employeePayrollList.add(empData);
-	        return empData;
-	    }
+	@Override
+	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = null;
+		empData = new EmployeePayrollData(employeePayrollList.size() + 1, empPayrollDTO);
+		employeePayrollList.add(empData);
+		return empData;
+	}
 
-	    @Override
-	    public EmployeePayrollData updateEmployeePayrollData(int employeeId, EmployeePayrollDTO empPayrollDTO) {
-	    	EmployeePayrollData empData = this.getEmployeePayrollDataById(employeeId);
-	    	empData.setName(empPayrollDTO.name);
-	        empData.setSalary(empPayrollDTO.salary);
-	        employeePayrollList.set(employeeId-1, empData);
-	        return empData;
-	    }
+	@Override
+	public EmployeePayrollData updateEmployeePayrollData(int employeeId, EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = this.getEmployeePayrollDataById(employeeId);
+		empData.setName(empPayrollDTO.name);
+		empData.setSalary(empPayrollDTO.salary);
+		employeePayrollList.set(employeeId - 1, empData);
+		return empData;
+	}
 
-	    @Override
-	    public void deleteEmployeePayrollData(int employeeId) {
-	    	employeePayrollList.remove(employeeId-1);
-	    }
+	@Override
+	public void deleteEmployeePayrollData(int employeeId) {
+		employeePayrollList.remove(employeeId - 1);
+	}
 
 }

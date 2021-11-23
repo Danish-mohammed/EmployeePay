cffbc1955d04590c16fc8bb58bee4d0bb478e513
AtifNusrath <mohammedatif90@gmail.com>
2021-11-23 21:55:24 +0530

    Added: CRUD service methods with MySQL Database

--- src/main/java/com/bridgelabz/demo/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/demo/model/EmployeePayrollData.java
@@ -10,45 +10,41 @@ import javax.persistence.*;
 @Table(name = "employeepayroll_db")
 public @Data class EmployeePayrollData {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "employee_id")
-    private int employeeId;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "salary")
-    private long salary;
-    @Column(name = "gender")
-    private String gender;
-    @Column(name = "start_date")
-    private LocalDate startDate;
-    @Column(name = "note")
-    private String note;
-    @Column(name = "profile_pic")
-    private String profilePic;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "employee_id")
+	private int employeeId;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "salary")
+	private long salary;
+	@Column(name = "gender")
+	private String gender;
+	@Column(name = "start_date")
+	private LocalDate startDate;
+	@Column(name = "note")
+	private String note;
+	@Column(name = "profile_pic")
+	private String profilePic;
 
-    @ElementCollection
-    @CollectionTable(name = "employee_department",
-            joinColumns = @JoinColumn(name ="id"))
-    @Column(name = "departments")
-    private List<String> departments;
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "departments")
+	private List<String> departments;
 
-	public EmployeePayrollData() {
-
+	public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+		this.updateEmployeePayollData(employeePayrollDTO);
 	}
 
-	public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO){
-        this.updateEmployeePayollData(employeePayrollDTO);
+	public void updateEmployeePayollData( EmployeePayrollDTO employeePayrollDTO) {
+        this.name = employeePayrollDTO.name;
+        this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.note = employeePayrollDTO.note;
+        this.startDate = employeePayrollDTO.startDate;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
+
     }
-	
-	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
-		this.employeeId = employeeId;
-		this.name = employeePayrollDTO.name;
-		this.salary = employeePayrollDTO.salary;
-		this.gender = employeePayrollDTO.gender;
-		this.startDate = employeePayrollDTO.startDate;
-		this.note = employeePayrollDTO.note;
-		this.profilePic = employeePayrollDTO.profilePic;
-		this.departments = employeePayrollDTO.departments;
-	}
+}
 
 }

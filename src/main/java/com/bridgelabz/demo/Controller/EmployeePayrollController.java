a39afd30925a8e2c3f8a1d02a90238903b1f7022
Danish-mohammed <94976088+Danish-mohammed@users.noreply.github.com>
2021-12-11 16:23:41 +0530

    Update EmployeePayrollController.java

--- src/main/java/com/bridgelabz/demo/Controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/demo/Controller/EmployeePayrollController.java
@@ -17,16 +17,16 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.bridgelabz.demo.Exceptions.EmployeePayrollException;
 import com.bridgelabz.demo.Service.IEmployeePayrollService;
 import java.util.List;
 import java.util.Optional;
-
 import lombok.extern.slf4j.Slf4j;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 @RequestMapping("/employeepayrollservice")
 @Slf4j
 public class EmployeePayrollController {
